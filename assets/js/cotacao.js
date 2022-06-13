a7a1a0783eb1e89d95db2156235291960a657c40
Otto Gugel <harpgamer31@gmail.com>
2022-06-13 18:42:49 -0300

    adicionando uma função do valor da moedaBitcoin

--- assets/js/cotacao.js
+++ assets/js/cotacao.js
@@ -1,4 +1,5 @@
 function load() {
+  // a cada 30 segundos atualiza o valor real do bitcoin
   const cotacaoBitcoin = async () => {
     const url = `https://blockchain.info/ticker` // json blockchain
     const moedas = await fetch(url)
@@ -8,3 +9,15 @@ function load() {
   cotacaoBitcoin()
   setInterval(cotacaoBitcoin, 30000)
 }
+
+const moedaBitcoin = () => {
+  // qualquer valor que seja atribuido ao 1 input, logo é alterado no 2 também.
+  const valorBitcoin = document.getElementById('estrangeiro')
+  let conversaoReais = document.getElementById('nacional')
+
+  valorBitcoin.addEventListener('input', () => {
+    conversaoReais.value = valorBitcoin.value * 143252.3
+  })
+}
+
+moedaBitcoin()
